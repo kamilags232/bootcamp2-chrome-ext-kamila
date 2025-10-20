ffcbd8e848805df2ce7d70deb1acbb0a89b49a81
kamilags232 <kamila.gs@sempreceub.com>
2025-10-19 22:09:08 -0300

    Modificações

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -2,14 +2,19 @@ import { defineConfig, devices } from '@playwright/test';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+// Corrige __dirname e __filename no modo ES Module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Caminho da extensão empacotada
 const distPath = path.join(__dirname, '..', 'dist');
 
 export default defineConfig({
   testDir: __dirname,
-  reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report' }]
+  ],
   use: {
     headless: true,
   },
